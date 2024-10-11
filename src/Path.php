22f19a2f750d1b73b068819d22f3bfa5a4465096
Shahzada Modassir <152081666+shahzadamodassir@users.noreply.github.com>
2024-10-11 18:56:39 +0530

    Add files via upload

--- src/Path.php
+++ src/Path.php
@@ -8,95 +8,12 @@ use Path\PathBuilder;
 
 class Path extends PathBuilder
 {
-  /**
-   * Returns a accurrate pathname, exclude with rootname Its used to extract [local] pathname.
-   * But not reducing '..' and '.' parts they're not replaced by a single one; when the path
-   * contains a trailing slash.
-   * 
-   * @param string $path [required]
-   * @return string local pathname
-   */
-  public static function pathname(string $path) : string
+
+  public static function dirname(string $path, int $levels = 1, string $suffix = '') : string
   {
-    $path = self::separate($path);
-    return \mb_substr($path, \mb_strlen(self::rootname($path)));
+    return self::suffix(\dirname($path, $levels), $suffix);
   }
 
-  /**
-   * On Windows systems only, returns an equivalent namespace-prefixed path for the given path.
-   * If path is not a string, path will be returned without modifications.
-   * This method is meaningful only on Windows system. On POSIX systems,
-   * the method is non-operational and always returns path without modifications.
-   * 
-   * @param string $path [required]
-   * @return string namespaced-path
-   */
-  public static function toNamespcedPath(string $path) : string
-  {
-    return self::namespaceWith(self::normalize($path));
-  }
-
-  /**
-   * Join all arguments together and normalize the resulting path string.
-   * 
-   * @param array<string|null> $paths [required]
-   * @return string joined path string
-   */
-  public static function join(string ...$paths) : string
-  {
-    return self::normalize(\join(self::BSEP, $paths));
-  }
-
-  /**
-   * Trim all right side directory separators, Its used to resolve path.
-   * 
-   * @param string $path
-   * @return string
-   */
-  public static function rTrimSep(string $path) : string
-  {
-    return !!self::pathname($path) ? \preg_replace(self::RTRIM_SEP, '$1', $path) : $path;
-  }
-
-  /**
-   * Returns the real filtered-paths sources to make direct usable path
-   * 
-   * @param array|string $paths [required]
-   * @return string|bool
-   */
-  public static function real($paths) : array
-  {
-    $paths = (array) $paths;
-    
-    foreach($paths as $i=>$path) {
-      if (!\realpath($path)) unset($paths[$i]);
-    }
-
-    return $paths;
-  }
-
-   /**
-   * Returns active directory current working directory, root directory
-   * If pass $isCur true then returns active current working directory
-   * Otherwise returns active root directory
-   * 
-   * @param bool $isCur [optional]
-   * @return string
-   */
-  public static function active(bool $isCur = false)
-  {
-    return $isCur ? \getcwd() : $_SERVER['DOCUMENT_ROOT'];
-  }
-
-  /**
-   * Returns the full paths to the trimmed separators of the last last index of files, and join with paths
-   * separators and names combination to make a real files formate
-   * 
-   * @param string[] $paths [required]
-   * @param string[] $names [required]
-   * 
-   * @return string[]
-   */
   public static function filePaths($paths, $names)
   {
     $files = [];
@@ -110,44 +27,6 @@ class Path extends PathBuilder
     return $files;
   }
 
-  /**
-   * Return the directory name of a path. Similar to the Unix dirname command.
-   * On Windows, both slash (/) and backslash (\) are used as directory
-   * separator character. In other environments, it is the forward slash (/).
-   * 
-   * @param string $path   [required]
-   * @param int    $levels [optional]
-   * @param string $suffix [optional]
-   * @param string $prefix [optional]
-   * 
-   * @return string dirname
-   */
-  public static function dirname(string $path, int $levels = 1, string $suffix = '') : string
-  {
-    return self::suffix(\dirname($path, $levels), $suffix);
-  }
-
-  /**
-   * Returns clean path or string, to replace multi slashes to single (/)
-   * slashes. And remove all current directory (./) seperator But do not
-   * Remove current directory (^./) seperator of first index of path
-   * 
-   * @param string $path [required]
-   * @return string clean path
-   */
-  public static function clean(string $path, ?string $sep=null) : string
-  {
-    return self::refresh($path, $sep ?? self::BSEP);
-  }
-
-  /**
-   * Returns a path string from an array-object - the opposite of parse().
-   * format method are same work as `phpinfo` method But there install the
-   * extra property `root` property to getting current root [dir] of path
-   * 
-   * @param string $path [required]
-   * @return array format LIKE (root, dir, base, ext, name)
-   */
   public static function format(array $pathObject) : string
   {
     $format = [];
@@ -165,42 +44,11 @@ class Path extends PathBuilder
     return self::separate($format);
   }
 
-  /**
-   * Normalize a string path, reducing '..' and '.' parts. When multiple slashes are found,
-   * they're replaced by a single one; when the path contains a trailing slash,
-   * it is preserved. On Windows backslashes are used.
-   * 
-   * @param string $path [required]
-   * @return string normalized path
-   */
-  public static function normalize(string $path) : string
-  {
-    return self::normalized(
-      self::rootname($path), self::pathname($path), [self::class, 'separate']
-    );
-  }
-
-  /**
-   * Returns extname method a path extension name from given path, Its used to get file ext
-   * 
-   * @param string $path [required]
-   * @return string extname
-   */
   public static function extname(string $path) : string
   {
     return \pathinfo($path)['extension'];
   }
 
-  /**
-   * Solve the relative path from {from} to {to} based on the current working directory.
-   * At times we have two absolute paths, and we need to derive the relative path from
-   * one to the other. This is actually the reverse transform of path.resolve.
-   * 
-   * @param string $from [required]
-   * @param string $to   [required]
-   * 
-   * @return string relative-path
-   */
   public static function relative(string $from, string $to) : string
   {
     // Create arrayable paths after resolving {from} and {to} path.
@@ -237,52 +85,17 @@ class Path extends PathBuilder
     return $relative;
   }
 
-  /**
-   * Returns a [root] directory LIKE `(eg: C:/ C: / //root/dir/)` extract
-   * from path,[root] directory repersent a System or Server active drive
-   * 
-   * @param string $path [required]
-   * @return string rootname
-   */
-  public static function rootname(string $path) : string
-  {
-    \preg_match(self::ROOT, $path, $matched);
-    return $matched[0];
-  }
-
-  /**
-   * Determines whether {path} is an absolute path. An absolute path will always resolve
-   * to the same location, regardless of the working directory.
-   * If the given {path} is a zero-length string, false will be returned.
-   * 
-   * @param string $path [required]
-   * @return boolean true/false
-   */
   public static function isAbsolute(string $path) : bool
   {
     $matched = self::rootname($path);
-    return !!$matched && !\str_ends_with($matched, ':');
+    return !!$matched && !\str_ends_with($matched, self::delimiter);
   }
 
-  /**
-   * Returns filename method an filename name from given path, Its used to get filename.
-   * 
-   * @param string $path [required]
-   * @return string filename
-   */
   public static function filename(string $path) : string
   {
     return \pathinfo($path)['filename'];
   }
 
-  /**
-   * Returns an array-object from a path string - the opposite of format().
-   * parse method are same work as `phpinfo` method But there install the
-   * extra property `root` property to getting current root [dir] of path
-   * 
-   * @param string $path [required]
-   * @return array format LIKE (root, dir, base, ext, name)
-   */
   public static function parse(string $path) : string
   {
     return [
@@ -294,54 +107,42 @@ class Path extends PathBuilder
     ];
   }
 
-  /**
-   * Returns seprated path string with seperator LIKE (\\) and (/) of single or multiple path
-   * And remove all current directory (./) seperator But do not Remove current directory
-   * (^./) seperator of first index of path
-   * 
-   * @param string|array $paths [required]
-   * @param string       $sep   [optional]
-   * 
-   * @return string separate path with seperator
-   */
-  public static function separate($paths, string $sep=self::BSEP) : string
-  {
-    $path = self::clean(\join($sep, (array) $paths));
-    return \str_replace([self::BSEP, self::FSEP], $sep, $path);
-  }
-
-  /**
-   * Returns trailing name component of path basename, Its used to find, read and write files.
-   * Return the last portion of a path. Similar to the Unix basename command.
-   * Often used to extract the file name from a fully qualified path.
-   * 
-   * @param string $path   [required]
-   * @param string $suffix [optional]
-   * 
-   * @return string basename
-   */
   public static function basename(string $path, string $suffix = '') : string
   {
     return \is_callable('basename') ? \basename($path, $suffix) : \pathinfo($path)['basename'];
   }
 
-  /**
-   * The right-most parameter is considered {to}. Other parameters are considered an array of {from}.
-   * Starting from leftmost {from} parameter, resolves {to} to an absolute path.
-   * If {to} isn't already absolute, {from} arguments are prepended in right to left order, until an
-   * absolute path is found. If after using all {from} paths still no absolute path is found,
-   * the current working directory is used as well. The resulting path is normalized, and trailing
-   * slashes are removed unless the path gets resolved to the root directory.
-   * 
-   * @param string $paths [required]
-   * @return string resolved-path
-   */
-  public static function resolve(string ...$paths) : string
+  public static function real($paths) : array
   {
-    \array_unshift($paths, self::active(true));
-    return self::rTrimSep(
-      self::normalize(self::getCompleteSource(\array_map([self::class, 'separate'], $paths)))
-    );
+    $paths = (array) $paths;
+    
+    foreach($paths as $i=>$path) {
+      if (!\realpath($path)) unset($paths[$i]);
+    }
+
+    return $paths;
+  }
+
+  public static function join(string ...$paths) : string
+  {
+    return self::normalize(\join(self::BSEP, $paths));
+  }
+
+  public static function pathname(string $path)
+  {
+    $path = self::separate($path);
+    return \substr($path, \strlen(self::rootname($path)));
+  }
+
+  public static function normalize(string $path)
+  {
+    return self::normalized(self::rootname($path), self::pathname($path));
+  }
+
+  public static function resolve(string ...$paths)
+  {
+    self::prepend($paths, \getcwd());
+    return self::normalize(self::makeResolver(self::separateMap($paths)));
   }
 }
 ?>
--- src/PathBuilder.php
+++ src/PathBuilder.php
@@ -6,80 +6,226 @@ namespace Path;
 
 abstract class PathBuilder
 {
-  /** @var string ROOT Use to get root directory first index of path LIKE /|//|C:|C:/ */
-  protected const ROOT = '/^(?:(?:\\\\\\\\|\/\/)[^\\\\\/]+[\\\\\/]+[^\\\\\/]+[\\\\\/]?|(?:[A-Z]:)*[\\\\\/]?)|/';
+  protected const ROOT = '/^(?:(?:[\\\\]{2})[^\\\\]+[\\\\]+[^\\\\]+[\\\\]?|(?:[A-Z]:)*[\\\\]?)|/';
 
-  /** @var string FSEP and BSEP forward separator and backword separator */
+  public const namespace = '\\\\?';
+  public const parent = '..';
+  public const current = '.';
   public const BSEP = '\\';
   public const FSEP = '/';
+  public const delimiter = ':';
+  public const UNC = '\\UNC\\';
+  public const sep = self::BSEP;
+  
+  private const CUR_DIR = '/(?:^(\.[\\\\]))|(?:([^.])\.([\\\\]))/';
+  private const MULTI_SEP = '/(?:^([\\\\]{2})(?=[^\\\\]+[\\\\]+[^\\\\]+[\\\\]?)|([\\\\])[\\\\]*)/';
 
-  /**
-   * Matches the current directory identifier [.] of path
-   * 
-   * @var string
-   */
-  private const CUR_DIR = '/(?:^\.[\\\\\/])|(?:([^.])\.([\\\\\/]))/';
-
-  /**
-   * Matches the parent directory identifier [..] of path
-   * 
-   * @var string
-   */
-  private const PARENT_DIR = '/^(?:\.\.)$/';
-
-  /**
-   * Right side directory separator timmer regular expression
-   * 
-   * @var string
-   */
-  protected const RTRIM_SEP = '/([^\\\\\/]+)(\\\\|\/)+$/';
-
-  /**
-   * Drive and multi directory separator matcher Regular Expression
-   * 
-   * @var string
-   */
-  private const MULTI_SEP = '/(?:^(\\\\|\/)(\1)(?=[^\\\\\/]+[\\\\\/]+[^\\\\\/]+[\\\\\/]?)|([\/\\\\])[\/\\\\]*)/';
-
-  /**
-   * Returns a complete array-source to ready to normalize path string, It's used to resolve path string
-   * Handle drives, trailing slashes and creating array-range
-   * 
-   * @param array $paths
-   * @return array
-   */
-  protected static function getCompleteSource(array $paths)
+  private static function match(string $pattern, string $subject, bool $returnMatches = false)
   {
-    /** @var string $root Getting a root directory */
-    /** @var int $i for Reverse loop */
-    /** @var array $path Store final modified path */
-    $root = self::getDrive($paths);
-    $i = \count($paths) - 1;
-    $newPaths = [];
+    $isMatched = \preg_match($pattern, self::nuturalize($subject), $matches);
+    return $returnMatches ? $matches : $isMatched;
+  }
 
-    // Go through the array, Adding each of the items to their new values
+  private static function replace($pattern, $replacement, $subject)
+  {
+    return \preg_replace($pattern, $replacement, self::nuturalize($subject));
+  }
+
+  private static function nuturalize(string $path)
+  {
+    return \str_replace(self::FSEP, self::BSEP, $path);
+  }
+
+  public static function rootname(string $path) : string
+  {
+    return self::match(self::ROOT, $path, true)[0];
+  }
+
+  public static function separate($paths, ?string $sep = null)
+  {
+    $path = self::serialize((array) $paths, true);
+    $sep = $sep == null ? self::BSEP : $sep;
+    return $sep !== self::BSEP ? \str_replace(self::BSEP, $sep, $path) : $path;
+  }
+
+  
+  public static function serialize(array $paths, bool $isClean = false)
+  {
+    return self::sanitize(\join(self::BSEP, $paths), $isClean);
+  }
+
+  public static function sanitize(string $path, bool $isClean = true)
+  {
+    $replacement = ['$1$2$3', (!$isClean ? '$1' : null).'$2'];
+    return self::replace([self::MULTI_SEP, self::CUR_DIR], $replacement, $path);
+  }
+
+  /**
+   * 
+   * @param array $path
+   * @return string|false
+   */
+  private static function getDrive(array $paths)
+  {
+    /** @var bool|string Path drive */
+    $output = false;
+    foreach($paths as $path) {
+      if (($drive = self::rootname($path)) && $drive !== self::BSEP) {
+        $output = \rtrim($drive, self::BSEP);
+      }
+    }
+    return $output;
+  }
+
+  private static function isNetwork(string $path)
+  {
+    return !!(self::match(self::MULTI_SEP, $path, true)[1] ?? false);
+  }
+
+  private static function addPrefix(string $root)
+  {
+    return $root != null ? \rtrim($root, self::BSEP).self::BSEP : null;
+  }
+
+  protected static function makeResolver(array $paths)
+  {
+    $root = self::getDrive($paths);
+    $isRoot = $root != null;
+    $i = \count($paths) - 1;
+    $resolved = [];
+    /**
+     * 
+     * @param string $root
+     * @param string $path
+     * 
+     * @return void|true
+     */
+    $isStopped = function($root, $path, &$resolved) {
+      if (($path && $path[0] === self::BSEP) ||
+        (self::isNetwork($root))) {
+        self::prepend($resolved, \substr($path, \strlen($root)));
+        return true;
+      }
+    };
+    /**
+     * 
+     * @param array  $resolved
+     * @param string $root
+     * @param bool   $isRoot
+     * 
+     * @return string
+     */
+    $output = function($resolved, $root, $isRoot) {
+      $isRoot && self::prepend($resolved, $root);
+      $r = self::serialize($resolved, true);
+      if (\count($resolved) > 1) {
+        return \rtrim($r, self::BSEP);
+      }
+      return $r;
+    };
+
     for(; $i >= 0; $i--) {
       $path = $paths[$i];
-
-      if (self::matchDrive($root, $path)) {
+      
+      if (self::verifyDrive($root, $path)) {
         if (self::getDrive([$path])) {
-          $path = \mb_substr($path, \mb_strlen($root));
+          $path = \substr($path, \strlen($root));
         }
-      // Otherwise skip the unmatched drive path
       } else {
         continue;
       }
 
-      // Fix reverse behaviour and insert path teek.
-      \array_unshift($newPaths, $path);
-
-      if ($path && ($path[0] === self::BSEP)) {
-        break;
+      self::prepend($resolved, $path);
+      
+      if ($isStopped($root, $path, $resolved)) {
+        break; // Stop Immediately loop
       }
     }
 
-    \array_unshift($newPaths, $root);
-    return \join(self::BSEP, $newPaths);
+    return $output($resolved, $root, $isRoot);
+  }
+
+  private static function createRegex(string $source)
+  {
+    return \sprintf('/^%s/', \str_replace(self::BSEP, '\\\\', $source));
+  }
+
+  private static function verifyDrive(string $target, string $matcher)
+  {
+    $matcherDrive = self::getDrive([$matcher]);
+    $targetDrive  = self::getDrive([$target]);
+    
+    if ($matcherDrive && self::isNetwork($matcherDrive)) {
+      return false;
+    }
+
+    return $targetDrive && $matcherDrive && self::match(self::createRegex($targetDrive), $matcherDrive) ||
+      !$matcherDrive;
+  }
+
+  /**
+   * 
+   * @param array $array
+   * @return mixed
+   */
+  private static function remove(array &$array)
+  {
+    return \array_pop($array);
+  }
+
+  /**
+   * 
+   * @param array $paths
+   * @return array
+   */
+  public static function separateMap(array $paths)
+  {
+    $output = [];
+    foreach($paths as $path) {
+      $output[] = self::separate($path);
+    }
+    return $output;
+  }
+
+  /**
+   * 
+   * @param string &$array
+   * @param mixed  $values
+   * 
+   * @return int
+   */
+  protected static function prepend(array &$array, $values)
+  {
+    return self::insert($array, $values, true);
+  }
+
+  /**
+   * 
+   * @param string &$array
+   * @param mixed  $values
+   * 
+   * @return int
+   */
+  protected static function append(array &$array, $values)
+  {
+    return self::insert($array, $values, false);
+  }
+
+  /**
+   * 
+   * 
+   * @param array &$array
+   * @param mixed $values
+   * @param bool  $isPrepend
+   * 
+   * @return int
+   */
+  private static function insert(array &$array, $values, $isPrepend)
+  {
+    $values = (array) $values;
+    foreach($values as $value) {
+      !!$isPrepend ? \array_unshift($array, $value) : \array_push($array, $value);
+    }
   }
 
   /**
@@ -87,108 +233,44 @@ abstract class PathBuilder
    * they're replaced by a single one; when the path contains a trailing slash,
    * it is preserved. On Windows backslashes are used.
    * 
-   * @param string $root     [required]
-   * @param string $path     [required]
-   * @param array  $separate [required]
+   * @param string $root [required]
+   * @param string $path [required]
    * 
-   * @return string
+   * @return string normalized path
    */
-  protected static function normalized(string $root, string $path, array $separate)
+  protected static function normalized(string $root, string $path) : string
   {
+    // TODO: fix current directory bugs of specific version
+    $path = $path==null ? self::current.self::BSEP : $path;
     $fDirs = \explode(self::BSEP, $path);
+    $root = self::addPrefix($root);
     $normalized = [];
     $tmpParents = [];
-    $endPart = end($fDirs);
+    $endPart = @end($fDirs);
     $hasContains = function(array $array) {
       foreach($array as $data) {
-        if (!!$data) {
+        if ($data != null) {
           return true;
         }
       }
       return false;
     };
-
+    
     foreach($fDirs as $fDir) {
-      if ($hasParent = \preg_match(self::PARENT_DIR, $fDir)) {
-        \array_pop($normalized)==null && \array_push($tmpParents, $fDir);
+      if ($fDir === self::parent) {
+        self::remove($normalized)==null && self::append($tmpParents, $fDir);
       } else {
-        \array_push($normalized, $fDir);
-      }
-    }
-
-    // Attach root dir If All Done!
-    if ($root) {
-      \array_unshift($normalized, $root);
-    } else {
-      \count($tmpParents) > 0 ? \array_unshift($normalized, ...$tmpParents) : !$hasContains($normalized) && !$endPart && \array_unshift($normalized, '.', $endPart);
-    }
-
-    return $separate($normalized);
-  }
-
-  /**
-   * Returns the refresh path or string to replace the multi slashes to single (/)
-   * slashes. And remove all current directory (./) seperator But do not
-   * Remove current directory (^./) seperator of first index of path
-   * 
-   * @param string $path [required]
-   * @return string refresh path
-   */
-  public static function refresh(string $path, ?string $sep=null) : string
-  {
-    $replacement = ['$1'.($sep ?? '$3'), '$1'];
-    return \preg_replace([self::MULTI_SEP, self::CUR_DIR], $replacement, $path);
-  }
-
-  /**
-   * Add namespace identifier '\\\\?' and includes '\UNC\' of specific drive
-   * And normalizing the path string.
-   * 
-   * @param string $path
-   * @return string
-   */
-  protected static function namespaceWith(string $path)
-  {
-    return '\\\\?'.\preg_replace_callback(self::MULTI_SEP, function($matched) {
-      return $matched[1] ? '\\UNC\\' : $matched[3] ?? $matched[2];
-    }, $path);
-  }
-  
-  /**
-   * Returns only matches path drive Otherwise return false, It used to gets drive
-   * 
-   * @param array $path
-   * @return string|false
-   */
-  protected static function getDrive(array $paths)
-  {
-    /** @var bool|string Path drive */
-    $output = false;
-    foreach($paths as $path) {
-      \preg_match(self::ROOT, $path, $matches);
-      $drive = $matches[0];
-      if (!!$drive && $drive !== self::BSEP) {
-        $output = \rtrim($drive, self::BSEP);
+        self::append($normalized, $fDir);
       }
     }
     
-    return $output;
-  }
-
-  /**
-   * There matches the same drive or relative path, Its used to making resolved array-source
-   * 
-   * @param string $target
-   * @param string $matcher
-   * 
-   * @return bool
-   */
-  protected static function matchDrive(string $target, string $matcher) : bool
-  {
-    $abspath = self::getDrive([$matcher]);
-    $drive   = self::getDrive([$target]);
-
-    return ($drive && $abspath && \preg_match(\sprintf('/^%s/', \str_replace(self::BSEP, '\\\\', $drive)), $abspath)) || !$abspath;
+    if ($root) {
+      self::prepend($normalized, $root);
+    } else {
+      \count($tmpParents) > 0 ? self::prepend($normalized, $tmpParents) : !$hasContains($normalized) && $endPart == null && self::prepend($normalized, self::current);
+    }
+    
+    return self::serialize($normalized);
   }
 
   /**
