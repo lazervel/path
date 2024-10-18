a25e12f90dd4aa37e3d260c3e6463389c9d94156
shahzadamodassir <shahzadamodassir@gmail.com>
2024-10-18 09:14:09 +0530

    added features

--- src/PathBuilder.php
+++ src/PathBuilder.php
@@ -288,6 +288,17 @@ abstract class PathBuilder extends PathHelper
   }
 
   /**
+   * 
+   * 
+   * @param string $path [required]
+   * @return string
+   */
+  public static function toNamespcedPath(string $path) : string
+  {
+
+  }
+
+  /**
    * Normalize a string path, reducing '..' and '.' parts. When multiple slashes are found,
    * they're replaced by a single one; when the path contains a trailing slash,
    * it is preserved. On Windows backslashes are used.
